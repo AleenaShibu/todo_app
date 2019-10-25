37a7501116a42071e8eb869a32911377cb743b4a
Aleena Shibu <aleenashibu94@gmail.com>
2019-10-25 15:18:00 +0530

    Created a todo app

---
+++ src/todosData.js
@@ -0,0 +1,14 @@
+const todosData = [
+    {
+        id: 1,
+        task: "Work Done",
+        completed: true
+    },
+    {
+        id: 2,
+        task: "Doing",
+        completed: false
+    }
+]
+export default todosData
+    
