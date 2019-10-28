bb2272550b164e84a26d4725f6914909f57951b9
Aleena Shibu <aleenashibu94@gmail.com>
2019-10-28 13:15:09 +0530

    Updates

---
+++ precache-manifest.0465b06993ddd8741967647a74150a40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20c5500da274dd259cb48c0ea3c4cc58",
+    "url": "/todo_app/index.html"
+  },
+  {
+    "revision": "34af842adef360977245",
+    "url": "/todo_app/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "15c373b4b18b114159f2",
+    "url": "/todo_app/static/js/2.21d446dd.chunk.js"
+  },
+  {
+    "revision": "34af842adef360977245",
+    "url": "/todo_app/static/js/main.4c7d7790.chunk.js"
+  },
+  {
+    "revision": "3de74b76c48534b1b641",
+    "url": "/todo_app/static/js/runtime-main.e9a0b223.js"
+  }
+]);
